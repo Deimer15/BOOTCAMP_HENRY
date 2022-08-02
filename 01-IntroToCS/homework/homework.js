d43cf8b05a9201fd84bdf01ab8846184a1e200da
Deimer15 <106549908+Deimer15@users.noreply.github.com>
2022-08-02 18:30:14 -0500

    Primer Commit

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function BinarioADecimal(num) {
+    // tu codigo aca
+    let sum = 0,
+        index = 0;
+    for (let i = num.length - 1; i >= 0; i--) {
+        sum += Math.pow(2, index) * parseInt(num[i], 10);
+        index++;
+    }
+    return sum;
+}
+
+function DecimalABinario(num) {
+    // tu codigo aca
+    let arr = [],
+        num_origi = num;
+    do {
+        arr.unshift(Math.floor(num % 2));
+        num = num / 2;
+    } while (num >= 1);
+    return arr.join('');
+}
+
+
+module.exports = {
+    BinarioADecimal,
+    DecimalABinario,
+}
