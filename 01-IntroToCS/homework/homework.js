0a4e9e2bbb39f4ea8800e888f092dabfd77a5c2e
Deimer15 <106549908+Deimer15@users.noreply.github.com>
2022-08-02 21:37:26 -0500

    Actualizacion 21:37 02/08/22

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,10 +3,21 @@
 function BinarioADecimal(num) {
     // tu codigo aca
     let sum = 0,
-        index = 0;
+        index = 0,
+        sum2 = 1;
     for (let i = num.length - 1; i >= 0; i--) {
-        sum += Math.pow(2, index) * parseInt(num[i], 10);
+        /*if (index === 0) {
+            sum2 = 1;
+        } else {
+            for (let j = 1; j <= index; j++) {
+                sum2 *= 2;
+            }
+        }*/
+        //sum += sum2 * parseInt(num[i], 10);
+        sum += 2 ** index * parseInt(num[i], 10);
+        //sum += Math.pow(2, index) * parseInt(num[i], 10);
         index++;
+        sum2 = 1;
     }
     return sum;
 }
